9548b8ee1726eeb173182238c579a92e058e06c9
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2021-03-28 22:45:23 +0200

    done

---
+++ Match.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Match.aspx.cs" Inherits="Exercice.Match" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Calenderier</h2>
+            <h2>Botola pro 2021</h2>
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                <asp:ListItem>selectionez match</asp:ListItem>
+                <asp:ListItem>Maroc VS USA</asp:ListItem>
+                <asp:ListItem>Canada Vs Brazil</asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        <asp:Calendar ID="Calendar1" runat="server" Visible="False"></asp:Calendar>
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        <h3>Changer la date et click sur le button pour le modifier</h3>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Changer Date" OnClick="Button1_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Match.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercice
+{
+    public partial class Match : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Calendar1.Visible = true;
+            switch (((DropDownList)DropDownList1).SelectedIndex)
+            {
+                case 1:
+                    Calendar1.SelectedDate = new DateTime(2021, 02, 20);
+                    Calendar1.VisibleDate = Calendar1.SelectedDate;
+                    Label1.Text=$"Match : {DropDownList1.Text}<br/>Le : {Calendar1.SelectedDate}";
+                    break;
+                case 2:
+                    Calendar1.SelectedDate = new DateTime(2021, 03, 20);
+                    Calendar1.VisibleDate = Calendar1.SelectedDate;
+                    Label1.Text = $"Match : {DropDownList1.Text}<br/>Le : {Calendar1.SelectedDate}";
+                    break;
+                    Calendar1.SelectedDate = new DateTime(2021, 04, 20);
+                    Calendar1.VisibleDate = Calendar1.SelectedDate;
+                    Label1.Text = $"Match : {DropDownList1.Text}<br/>Le : {Calendar1.SelectedDate}";
+                case 3:
+                    break;
+                default:
+                    Calendar1.Visible = false;
+                    Label1.Text = "aucun match selectione";
+                    break;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Calendar1.SelectedDate = new DateTime(2023, 10, 02);
+            Calendar1.VisibleDate = Calendar1.SelectedDate;
+            Label1.Text = $"Match : {DropDownList1.Text}<br/>Le : {Calendar1.SelectedDate}";
+        }
+    }
+}
